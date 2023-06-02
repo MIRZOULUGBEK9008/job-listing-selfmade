e1355562ffbc0a2be8f73e9a88aaa840d9b7a4fc
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2023-06-02 09:28:17 +0500

    Fix template bug and change some images' name

--- js/main.js
+++ js/main.js
@@ -2,40 +2,69 @@
 const elBody = document.querySelector(".js-body"),
 elLoader = elBody.querySelector(".js-loader"),
 elMainContent = elBody.querySelector(".js-main-content"),
-elJobList = elBody.querySelector(".js-job-list"),
 templateJob = elBody.querySelector(".js-job-template"), // template
-elJobElementImg = templateJob.querySelector(".js-job-element-img"),
-elCompanyName = templateJob.querySelector(".js-company-name"),
-elBadgeNew = templateJob.querySelector(".js-badge-new"),
-elBadgeFeatured = templateJob.querySelector(".js-badge-featured"),
-elJobTitle = templateJob.querySelector(".js-job-title"),
-elExtraInfo = templateJob.querySelector(".js-extra-info"),
-elStatusList = templateJob.querySelector(".js-status-list"),
-templateStatus = elBody.querySelector(".js-status-template"), // template
-elStatusItem = templateStatus.querySelector(".js-status-item");
+elJobList = elBody.querySelector(".js-job-list")
 
-// VARIABLES
-// const api = "http://localhost:3000/jobList";
-
-
-// async function getLocalData() {
-//   const response = await fetch(api);
-//   const localData = await response.json();
-//   return localData;
-//   updateUserInterface(localData)
-// };
-
-// getLocalData();
-
-// function updateUserInterface(item) {
-//   item.forEach((item)=> {
-//     elJobList.appendChild(templateJob)
-//   });
-// }
-
-
+// LOADER
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(() => {
     elLoader.classList.add("loader-wrapper--none")
   }, 800);
 });
+
+// VARIABLES
+const api = "http://localhost:3000/jobList";
+
+
+// GET LOCAL DATA
+async function getLocalData() {
+  const response = await fetch(api);
+  const localData = await response.json();
+  updateUserInterface(localData);
+  return localData;
+};
+
+let templateJobContent = templateJob.content;
+let elFragment = document.createDocumentFragment();
+
+function updateUserInterface(item) {
+  item.forEach((item)=> {
+    const {
+      company,
+      logo,
+      newBadge,
+      featuredBadge,
+      position,
+      role,
+      level,
+      postedAt,
+      contract,
+      location,
+      languages,
+      tools
+    } = item;
+
+    let templateItem = templateJobContent.cloneNode(true);
+
+    const elJobItem = templateItem.querySelector(".js-item-job");
+    const elJobElementImg = templateItem.querySelector(".js-job-element-img");
+    const elCompanyName = templateItem.querySelector(".js-company-name");
+    const elBadgeNew = templateItem.querySelector(".js-badge-new");
+    const elBadgeFeatured = templateItem.querySelector(".js-badge-featured");
+    const elJobTitle = templateItem.querySelector(".js-job-title");
+    const elExtraInfo = templateItem.querySelector(".js-extra-info");
+
+
+    // STATUS
+    const status = ((role + ",").split() + (level + ",").split() + (languages + ",") + tools).split(","),
+    statusResult = status.filter((e) => e !== '');
+
+    elJobTitle.textContent = position;
+    elJobElementImg.src = logo
+
+    elFragment.appendChild(templateItem);
+  });
+  elJobList.appendChild(elFragment)
+};
+
+getLocalData();
